5b59e32f5b0286bf086d40860b6b22492443203f
sguha-fc <sahasrangshu@fusioncharts.com>
2015-04-17 15:05:21 +0530

    question work started

--- api/database.js
+++ api/database.js
@@ -1,4 +1,30 @@
 // this file contains all the functionalities on databse
+/**
+*
+	************** here is the table structures ***************
+	category { // table to hold the category details
+		categoryId : "",
+		categoryName : ""
+	}
+	question { // table to hold the question details
+		questionId : "",
+		questionStatement : "",
+		option1 : "",
+		option2 : "",
+		option3 : "",
+		option4 : "",
+		option5 : "",
+		answer : "",
+		dificulty : "",// 1-easy, 2-moderate, 3-tough, 4-for experts, default 2
+		createdOn : "",
+		createdBy : ""
+	}
+	questionToCategory { // table to hold the relationship of question to category
+		questionId : "",
+		categoryId : ""
+	}
+
+*/
 module.exports = {
 	databseName : "",
 	locallydb : require('locallydb'),// creating localdb instance
@@ -10,5 +36,11 @@ module.exports = {
 		this.databaseName = trimmedDatabaseName;
 		var db = new this.locallydb('./'+databaseName+);
 		return true;
+	}
+	user : {},
+	question : {
+		insertQuestionToDatabase : function(questionObject) {
+			
+		}
 	}
 };
---
+++ api/questions.js
@@ -0,0 +1,28 @@
+/**
+*
+	category { // table to hold the category details
+		categoryId : "",
+		categoryName : ""
+	}
+	question { // table to hold the question details
+		questionId : "",
+		questionStatement : "",
+		option1 : "",
+		option2 : "",
+		option3 : "",
+		option4 : "",
+		option5 : "",
+		answer : "",
+		dificulty : "",// 1-easy, 2-moderate, 3-tough, 4-for experts, default 2
+		createdOn : "",
+		createdBy : ""
+	}
+	questionToCategory { // table to hold the relationship of question to category
+		questionId : "",
+		categoryId : ""
+	}
+
+*/
+module.exports = {
+	
+};
